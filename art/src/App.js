6df8bdef9d1aeb0501aa620406bdf8034233696d
tnrxtms1 <138252332+tnrxtms1@users.noreply.github.com>
2023-07-07 11:40:08 -0700

    Updating branch with new libraries from MUI

--- art/src/App.js
+++ art/src/App.js
@@ -1,12 +1,16 @@
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 import App_header from "./components/App_header/App_header";
-import App_Footer from "./components/App_footer/App_footer";
+import App_footer from "./components/App_footer/App_footer";
 
 function App() {
   return (
     <>
   <div className="App">
   <App_header/>
-  <App_Footer/>
+  <App_footer/>
   </div>
     </>
   );
