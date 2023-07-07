1987df8eb790ab93ca739c7a2bef246c1e0f0c4c
tnrxtms1 <138252332+tnrxtms1@users.noreply.github.com>
2023-07-07 12:30:07 -0700

    Preparing to merge Ricks branch updates

--- art/src/App.js
+++ art/src/App.js
@@ -1,3 +1,4 @@
+import './App.css';
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
--- art/src/components/App_footer/App_footer.jsx
+++ art/src/components/App_footer/App_footer.jsx
@@ -1,19 +1,21 @@
 import React from 'react'
 import "./App_footer.css";
+import BottomNavigation from '@mui/material/BottomNavigation';
+import BottomNavigationAction from '@mui/material/BottomNavigationAction';
 
 function App_footer() {
   return (
-<BottomNavigation
-  showLabels
-  value={value}
-  onChange={(event, newValue) => {
-    setValue(newValue);
-  }}
->
-  <BottomNavigationAction label="Recents" icon={<RestoreIcon />} />
-  <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} />
-  <BottomNavigationAction label="Nearby" icon={<LocationOnIcon />} />
-</BottomNavigation>  )
+  <BottomNavigation
+    showLabels
+    value={value}
+    onChange={(event, newValue) => {
+      setValue(newValue);
+    }}
+  >
+    <BottomNavigationAction label="Recents" icon={<RestoreIcon />} />
+    <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} />
+    <BottomNavigationAction label="Nearby" icon={<LocationOnIcon />} />
+  </BottomNavigation>  )
 }
 
 export default App_footer
