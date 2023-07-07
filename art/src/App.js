6c46ee430bd4f7e2fb36c9c64fde08fa27fd8eef
Rick-Smart <rick-s@hotmail.com>
2023-07-07 10:14:38 -0700

    refreshing branch

--- art/src/App.js
+++ art/src/App.js
@@ -1,8 +1,14 @@
-import logo from "./logo.svg";
 import "./App.css";
+import AppHeader from "./components/AppHeader";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <>
+      <div className="App">
+        <AppHeader />
+      </div>
+    </>
+  );
 }
 
 export default App;
---
+++ art/src/components/AppHeader.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function AppHeader() {
+  return <div></div>;
+}
+
+export default AppHeader;
