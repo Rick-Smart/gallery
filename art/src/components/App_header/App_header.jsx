67da19880ec707280b3be1c493953b3d3d6dce0d
Rick-Smart <rick-s@hotmail.com>
2023-07-07 18:55:44 -0700

    updating from tanner branch

--- art/src/components/App_header/App_header.jsx
+++ art/src/components/App_header/App_header.jsx
@@ -1,41 +1,27 @@
-<<<<<<< HEAD
-import React from "react";
-import "./App_header.css";
-
-import Button from "@mui/material/Button";
-
-function App_header() {
-  return (
-    <>
-      <Button variant="text" disabled={true}>
-        Button
-      </Button>
-    </>
-  );
-}
-
-export default App_header;
-=======
-import * as React from 'react';
-import Typography from '@mui/material/Typography';
-import Breadcrumbs from '@mui/material/Breadcrumbs';
-import Link from '@mui/material/Link';
-import HomeIcon from '@mui/icons-material/Home';
-import WhatshotIcon from '@mui/icons-material/Whatshot';
-import MessageIcon from '@mui/icons-material/Message';
+import * as React from "react";
+import Typography from "@mui/material/Typography";
+import Breadcrumbs from "@mui/material/Breadcrumbs";
+import Link from "@mui/material/Link";
+import HomeIcon from "@mui/icons-material/Home";
+import WhatshotIcon from "@mui/icons-material/Whatshot";
+import MessageIcon from "@mui/icons-material/Message";
 
 function handleClick(event) {
   event.preventDefault();
-  console.info('You clicked a breadcrumb.');
+  console.info("You clicked a breadcrumb.");
 }
 
 export default function App_tester() {
   return (
     <div role="presentation" onClick={handleClick}>
-      <Breadcrumbs separator="|" aria-label="breadcrumb" sx={{"& ol": {justifyContent: "center", margin: "auto"}}}>
+      <Breadcrumbs
+        separator="|"
+        aria-label="breadcrumb"
+        sx={{ "& ol": { justifyContent: "center", margin: "auto" } }}
+      >
         <Link
           underline="hover"
-          sx={{ display: 'center', alignItems: 'center' }}
+          sx={{ display: "center", alignItems: "center" }}
           color="inherit"
           href="/"
         >
@@ -44,7 +30,7 @@ export default function App_tester() {
         </Link>
         <Link
           underline="hover"
-          sx={{ display: 'center', alignItems: 'center' }}
+          sx={{ display: "center", alignItems: "center" }}
           color="inherit"
           href="/material-ui/getting-started/installation/"
         >
@@ -53,7 +39,7 @@ export default function App_tester() {
         </Link>
         <Link
           underline="hover"
-          sx={{ display: 'center', alignItems: 'center' }}
+          sx={{ display: "center", alignItems: "center" }}
           color="text.primary"
           href="/material-ui/getting-started/installation/"
         >
@@ -64,4 +50,3 @@ export default function App_tester() {
     </div>
   );
 }
->>>>>>> 4018b361caf877c841d0fc19de7e1a8d585237e4
